7049765b87a8f26474b555809eb5c14750642463
Hannah Patricia Samson <hbsamson@up.edu.ph>
2026-08-07 09:09:19 +0800

    feat: added exit in main

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,25 @@ public class Main {
                 System.out.println("╠══════════════════════════════╣");
                 System.out.println("║  [1] War                     ║");
                 System.out.println("║  [2] Solitaire               ║");
+                System.out.println("║  [0] Exit                    ║");
                 System.out.println("╚══════════════════════════════╝");
 
                 do {
                     choice = input.askMenuChoice();
-                    if (choice != 1 && choice != 2) {
-                        System.out.println("\tInvalid choice. Please enter 1 or 2.");
+                    if (choice != 1 && choice != 2 && choice != 0 ) {
+                        System.out.println("\tInvalid choice. Please enter 1 or 2 to select a game, or 0 to exit.");
                     }
-                } while (choice != 1 && choice != 2);
+                } while (choice != 1 && choice != 2 && choice != 0 );
+
+                if (choice == 0) {
+                    System.out.println("\nExiting... Thanks for playing! <3");
+                    System.exit(0);
+                }
 
                 Game game = switch (choice) {
                     case 1 -> new WarGame(input);
                     case 2 -> new SolitaireGame(input);
-                    default -> throw new IllegalArgumentException("Invalid game selection.");
+                    default -> throw new IllegalArgumentException("Invalid selection.");
                 };
 
                 game.initialize();
